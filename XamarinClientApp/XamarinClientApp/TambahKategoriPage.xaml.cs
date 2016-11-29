4e948387decfe65ced83466a1ae7219db4a9fc68
erick <erick@DESKTOP-I963S2R>
2016-11-29 09:15:45 +0700

    menambahkan method tambah kategori

--- XamarinClientApp/XamarinClientApp/TambahKategoriPage.xaml.cs
+++ XamarinClientApp/XamarinClientApp/TambahKategoriPage.xaml.cs
@@ -5,6 +5,9 @@ using System.Text;
 using System.Threading.Tasks;
 
 using Xamarin.Forms;
+using XamarinClientApp.Models;
+using RestSharp.Portable.HttpClient;
+using RestSharp.Portable;
 
 namespace XamarinClientApp
 {
@@ -13,6 +16,30 @@ namespace XamarinClientApp
         public TambahKategoriPage()
         {
             InitializeComponent();
+
+            btnTambahKategori.Clicked += BtnTambahKategori_Clicked;
+        }
+
+        private RestClient _client =
+            new RestClient("http://contohinventorydb.azurewebsites.net/");
+
+        private async void BtnTambahKategori_Clicked(object sender, EventArgs e)
+        {
+            var _request = new RestRequest("api/Kategori", Method.POST);
+            var newKategori = new Kategori { NamaKategori = txtNamaKategori.Text };
+            _request.AddBody(newKategori);
+            try
+            {
+                var _response = await _client.Execute(_request);
+                if(_response.StatusCode==System.Net.HttpStatusCode.OK)
+                {
+
+                }
+            }
+            catch (Exception ex)
+            {
+                await DisplayAlert("Error", "Error : " + ex.Message , "OK");
+            }
         }
     }
 }
