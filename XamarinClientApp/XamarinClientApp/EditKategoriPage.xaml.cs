787f727b040b7010e19ad443f9b74117576a7e24
erick <erick@DESKTOP-I963S2R>
2016-11-29 12:02:42 +0700

    menambahkan delete barang

--- XamarinClientApp/XamarinClientApp/EditKategoriPage.xaml.cs
+++ XamarinClientApp/XamarinClientApp/EditKategoriPage.xaml.cs
@@ -19,10 +19,30 @@ namespace XamarinClientApp
             InitializeComponent();
 
             btnEdit.Clicked += BtnEdit_Clicked;
+            btnDelete.Clicked += BtnDelete_Clicked;
         }
 
         private RestClient _client =
-           new RestClient("http://contohinventorydb.azurewebsites.net/");
+          new RestClient("http://contohinventorydb.azurewebsites.net/");
+
+        private async void BtnDelete_Clicked(object sender, EventArgs e)
+        {
+            var _request = new RestRequest("api/Kategori/{id}", Method.DELETE); 
+            
+            _request.AddParameter("id",txtIdKategori.Text);
+            try
+            {
+                var _response = await _client.Execute(_request);
+                if (_response.StatusCode == System.Net.HttpStatusCode.OK)
+                {
+                    await Navigation.PopAsync();
+                }
+            }
+            catch (Exception ex)
+            {
+                await DisplayAlert("Error", "Error : " + ex.Message, "OK");
+            }
+        }
 
         private async void BtnEdit_Clicked(object sender, EventArgs e)
         {
